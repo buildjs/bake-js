f83c97dd5629b60c636c8a01633a02d40570d480
Damon Oehlman <damon.oehlman@sidelab.com>
2012-03-26 00:43:17 +1000

    Added spin.js recipe

---
+++ library/recipes/spin.js
@@ -0,0 +1,7 @@
+# dsc: A spinning activity indicator
+# author: fgnass
+# url: http://fgnass.github.com/spin.js
+# src: https://github.com/fgnass/spin.js
+# bug: https://github.com/fgnass/spin.js/issues
+
+github://fgnass/spin.js/spin.js

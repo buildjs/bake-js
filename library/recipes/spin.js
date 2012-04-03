87ac93c41cb1b59b7c858cb55492c01eda249141
Damon Oehlman <damon.oehlman@sidelab.com>
2012-04-03 16:45:14 +1000

    Moved recipes to a separate repo

--- library/recipes/spin.js
+++
@@ -1,7 +0,0 @@
-# dsc: A spinning activity indicator
-# author: fgnass
-# url: http://fgnass.github.com/spin.js
-# src: https://github.com/fgnass/spin.js
-# bug: https://github.com/fgnass/spin.js/issues
-
-https://raw.github.com/fgnass/spin.js/gh-pages/spin.js
